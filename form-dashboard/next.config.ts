b93c330dddd08ff168ee38754fa7c07193d1e144
gabrieulevi <gabriel@newnet.com.br>
2025-08-29 16:49:27 -0300

    tentando resolver problema parte 1

--- form-dashboard/next.config.ts
+++ form-dashboard/next.config.ts
@@ -1,6 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: "standalone", // Add this line
   allowedDevOrigins: ["http://localhost:3000", "http://0.0.0.0:3000"],
 };
 
