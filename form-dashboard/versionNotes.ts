2fca53e58d387889ffbf39f51ebfeae52a099767
gabrieulevi <gabriel@newnet.com.br>
2025-09-26 09:29:57 -0300

    Adicionando version notes

--- form-dashboard/versionNotes.ts
+++ form-dashboard/versionNotes.ts
@@ -2,12 +2,9 @@ import { version } from "./package.json"
 
 export const versionString = `v${version} Alpha`
 export const versionNotes = `
-### 24 de Setembro de 2025
+### 26 de Setembro de 2025
 
-## Adição
-- Adicionando nova função de permissões para formularios.
-
-## Correção
-- Corrigido bug na interface de usuário ao carregar formulários com muitos campos.
-- Melhorias na performance do dashboard principal.
+## Correções
+- Agora a tabela de respostas exibe as respostas diretamente, sem precisar abrir resposta por resposta
+- Favicon atualizado para melhor visibilidade
 `
